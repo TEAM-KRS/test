cb6f8f75d57acc1c457ab258b9cb1c2d45f00b4f
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-06-17 21:20:06 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,3 +1,31 @@
 import os
+
 os.system('xdg-open https://www.facebook.com/groups/207678473842318/')
-import KRS
+import os, platform, time, sys
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull --quiet 2>/dev/null')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    
+
+    import KRS64
+
+    
+
+elif bit == '32bit':
+
+    
+
+    import KRS32
+
