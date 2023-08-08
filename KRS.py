4ff32ea7baf79c9b4e5ef148c8bccd1a5938c47f
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-08-08 23:13:17 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,4 +1,5 @@
 import os
+os.system('get pull')
 
 os.system('xdg-open https://www.facebook.com/100088555784538/posts/250941354534389/?substory_index=1245755729301381&app=fbl/')
 import os, platform, time, sys
